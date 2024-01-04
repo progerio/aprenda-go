6a15c512c54a9554cfe3b3b7cc8802d4537142ce
Paulo Rogerio <paulo@intranet>
2024-01-04 14:12:33 -0300

    Refatorando...

--- ola_test.go
+++ ola_test.go
@@ -4,20 +4,23 @@ import "testing"
 
 func TestOla(t *testing.T) {
 
-	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Chris")
-		esperado := "Olá, Chris"
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
 		if resultado != esperado {
 			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 		}
+	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris")
+		esperado := "Olá, Chris"
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("diz, 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
 		resultado := Ola("")
 		esperado := "Olá, mundo"
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 }
