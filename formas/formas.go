da9b75d31bb4d32af5ee01147ba6755678c5d3d0
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2024-01-13 12:31:19 -0300

    Aprendendo sobre métodos e interface em go

--- formas/formas.go
+++ formas/formas.go
@@ -1,14 +1,30 @@
 package formas
 
+import "math"
+
 type Retangulo struct {
 	Largura float64
 	Altura  float64
+}
+
+// metodo do area do retangulo
+func (r Retangulo) Area() float64 {
+	return r.Largura * r.Altura
+}
+
+type Circulo struct {
+	Raio float64
+}
+
+// metodo area do circulo
+func (c Circulo) Area() float64 {
+	return math.Pi * c.Raio * c.Raio
 }
 
 func Perimetro(retangulo Retangulo) float64 {
 	return 2 * (retangulo.Largura + retangulo.Altura)
 }
 
-func Area(retangulo Retangulo) float64 {
-	return retangulo.Largura * retangulo.Altura
+type Forma interface {
+	Area() float64
 }
--- formas/formas_test.go
+++ formas/formas_test.go
@@ -13,11 +13,21 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	retangulo := Retangulo{12.0, 6.0}
-	resultado := Area(retangulo)
-	esperado := 72.0
-
-	if resultado != esperado {
-		t.Errorf("resultado %.2f esperado %.2f", resultado, esperado)
+	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
+		t.Helper()
+		resultado := forma.Area()
+		if resultado != esperado {
+			t.Errorf("resultado %.2f esperado %.2f", resultado, esperado)
+		}
 	}
+
+	t.Run("retangulos", func(t *testing.T) {
+		retangulo := Retangulo{12.0, 6.0}
+		verificaArea(t, retangulo, 72.0)
+	})
+
+	t.Run("circulos", func(t *testing.T) {
+		circulo := Circulo{10}
+		verificaArea(t, circulo, 314.1592653589793)
+	})
 }
