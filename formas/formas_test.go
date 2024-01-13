c516750e44af0899671da3c24190a050fca12a52
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2024-01-13 12:48:25 -0300

    Refatorando os testes de forma. Para receber parametros nomeados.

--- formas/formas_test.go
+++ formas/formas_test.go
@@ -17,9 +17,9 @@ func TestArea(t *testing.T) {
 		forma    Forma
 		esperado float64
 	}{
-		{Retangulo{12, 6}, 72.0},
-		{Circulo{10}, 314.1592653589793},
-		{Triangulo{12, 6}, 36.0},
+		{forma: Retangulo{Largura: 12, Altura: 6}, esperado: 72.0},
+		{forma: Circulo{Raio: 10}, esperado: 314.1592653589793},
+		{forma: Triangulo{Base: 12, Altura: 6}, esperado: 36.0},
 	}
 
 	for _, tt := range testesArea {
