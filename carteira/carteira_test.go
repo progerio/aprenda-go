9dbcb598ed2e2335a229b588f7a48bdcb607ef03
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2024-01-14 20:29:56 -0300

    Refatorando test da carteira

--- carteira/carteira_test.go
+++ carteira/carteira_test.go
@@ -1,29 +1,26 @@
 package carteira
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestCarteira(t *testing.T) {
+	confirmaSaldo := func(t *testing.T, carteira Carteira, valorEsperado Bitcoin) {
+		t.Helper()
+		resultado := carteira.Saldo()
+
+		if resultado != valorEsperado {
+			t.Errorf("resultado %s, esperado %s", resultado, valorEsperado)
+		}
+	}
 	t.Run("Depositar", func(t *testing.T) {
 		carteira := Carteira{}
 		carteira.Depositar(Bitcoin(10))
-		resultado := carteira.Saldo()
-		fmt.Printf("O endereço  do saldo no teste é %v \n", &carteira.saldo)
-		esperado := Bitcoin(10)
-
-		if resultado != esperado {
-			t.Errorf("resultado %d, esperado %d", resultado, esperado)
-		}
+		confirmaSaldo(t, carteira, Bitcoin(10))
 	})
 	t.Run("Retirar", func(t *testing.T) {
 		carteira := Carteira{saldo: Bitcoin(20)}
 		carteira.Retirar(Bitcoin(10))
-		resultado := carteira.Saldo()
-		esperado := Bitcoin(10)
-		if resultado != esperado {
-			t.Errorf("resultado %d, esperado %d", resultado, esperado)
-		}
+		confirmaSaldo(t, carteira, Bitcoin(10))
 	})
 }
