f1e2305a0311b6bd049beb34d8e42d7e2c3ed5fb
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2024-01-14 20:09:41 -0300

    Refatorando a carteira usando um tipo diferente

--- carteira/carteira.go
+++ carteira/carteira.go
@@ -2,15 +2,17 @@ package carteira
 
 import "fmt"
 
+type Bitcoin int
+
 type Carteira struct {
-	saldo int
+	saldo Bitcoin
 }
 
-func (c *Carteira) Depositar(quantidade int) {
+func (c *Carteira) Depositar(quantidade Bitcoin) {
 	fmt.Printf("O endereço do saldo no Depositar é %v \n", &c.saldo)
 	c.saldo += quantidade
 }
 
-func (c *Carteira) Saldo() int {
+func (c *Carteira) Saldo() Bitcoin {
 	return c.saldo
 }
--- carteira/carteira_test.go
+++ carteira/carteira_test.go
@@ -7,10 +7,10 @@ import (
 
 func TestCarteira(t *testing.T) {
 	carteira := Carteira{}
-	carteira.Depositar(10)
+	carteira.Depositar(Bitcoin(10))
 	resultado := carteira.Saldo()
 	fmt.Printf("O endereço  do saldo no teste é %v \n", &carteira.saldo)
-	esperado := 10
+	esperado := Bitcoin(10)
 
 	if resultado != esperado {
 		t.Errorf("resultado %d, esperado %d", resultado, esperado)
