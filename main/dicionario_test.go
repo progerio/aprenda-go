370810c6ecfbbcc5532dd3d37941a1ac11705fc3
Paulo Rogerio <526290+progerio@users.noreply.github.com>
2024-01-16 23:04:30 -0300

    Refatorando metodos

--- main/dicionario_test.go
+++ main/dicionario_test.go
@@ -7,7 +7,11 @@ func TestBusca(t *testing.T) {
 
 	resultado := Busca(dicionario, "teste")
 	esperado := "isso é apenas um teste"
+	comparaStrings(t, resultado, esperado)
+}
 
+func comparaStrings(t *testing.T, resultado, esperado string) {
+	t.Helper()
 	if resultado != esperado {
 		t.Errorf("resultado %s, esperado %s, dado %s", resultado, esperado, "test")
 	}
