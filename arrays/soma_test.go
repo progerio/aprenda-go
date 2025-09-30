b256c68d81d3821536f6b700cf0431f80c4ba49d
Paulo Rogerio <paulo@intranet>
2025-09-30 07:47:16 -0300

    Aprendendo arrays and slices

--- arrays/soma_test.go
+++ arrays/soma_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 	t.Run("soma de um array de inteiros", func(t *testing.T) {
@@ -21,5 +24,34 @@ func TestSoma(t *testing.T) {
 		if esperado != resultado {
 			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
 		}
+	})
+}
+
+func TestSomaTudo(t *testing.T) {
+	recebido := SomaTudo([]int{1, 3}, []int{0, 9})
+
+	esperado := []int{4, 9}
+	if !reflect.DeepEqual(recebido, esperado) {
+		t.Errorf("resultado %v, esperado %v", recebido, esperado)
+	}
+}
+func TestSomaTodoOResto(t *testing.T) {
+
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("faz as somas do resto", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+		verificaSomas(t, resultado, esperado)
+	})
+	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+		verificaSomas(t, resultado, esperado)
 	})
 }
