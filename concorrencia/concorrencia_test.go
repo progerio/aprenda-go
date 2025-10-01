77f6d0df4eda9c24b8740cb62205c834bb6a08c0
Paulo Rogerio <paulo@intranet>
2025-10-01 14:46:16 -0300

    feat: Add VerificadorWebSites function and corresponding tests

---
+++ concorrencia/concorrencia_test.go
@@ -0,0 +1,49 @@
+package concorrencia
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockVerificadorWebSite(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestVerificadorWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+	esperado := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	resultado := VerificadorWebSites(mockVerificadorWebSite, websites)
+
+	if !reflect.DeepEqual(esperado, resultado) {
+		t.Fatalf("esperado %v, recebido %v", esperado, resultado)
+	}
+}
+
+func slowStubVerifiadorWebsite(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkVerificadorWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		urls[i] = "http://www.google.com"
+	}
+
+	for i := 0; i < b.N; i++ {
+		VerificadorWebSites(slowStubVerifiadorWebsite, urls)
+	}
+}
