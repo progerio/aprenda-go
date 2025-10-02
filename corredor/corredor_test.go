dadb0729614590c0c70fab94fe2326cc54b194c1
Paulo Rogerio <paulo@intranet>
2025-10-02 09:37:30 -0300

    refactor: Enhance Corredor and Configuravel functions to return error handling

--- corredor/corredor_test.go
+++ corredor/corredor_test.go
@@ -8,20 +8,31 @@ import (
 )
 
 func TestCorredor(t *testing.T) {
-	servidorLento := criarServidorcomAtraso(20 * time.Millisecond)
-	servidorRapido := criarServidorcomAtraso(0 * time.Millisecond)
+	t.Run("compara a velocidade de servidores, retornando o endereço do mais rápido", func(t *testing.T) {
 
-	servidorLento.Close()
-	servidorRapido.Close()
+		servidorLento := criarServidorcomAtraso(20 * time.Millisecond)
+		servidorRapido := criarServidorcomAtraso(0 * time.Millisecond)
 
-	URLLenta := servidorLento.URL
-	URLRapida := servidorRapido.URL
+		servidorLento.Close()
+		servidorRapido.Close()
 
-	esperado := URLRapida
-	resultado := Corredor(URLLenta, URLRapida)
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-	}
+		_, err := Corredor(servidorLento.URL, servidorRapido.URL)
+
+		if err != nil {
+			t.Error("esperava um erro, mas não obtive um")
+		}
+	})
+
+	t.Run("retorna um erro se o tempo limite for excedido", func(t *testing.T) {
+		servidor := criarServidorcomAtraso(25 * time.Millisecond)
+		defer servidor.Close()
+
+		_, err := Configuravel(servidor.URL, servidor.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("esperava um erro, mas não obtive um")
+		}
+	})
 }
 
 func criarServidorcomAtraso(duracao time.Duration) *httptest.Server {
