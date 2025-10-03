acb983c5c7fbadaca351c1cfef5184647477ba24
Paulo Rogerio <paulo@intranet>
2025-10-03 13:09:47 -0300

    refactor: Revise TestPercorre to enhance test cases and improve map handling

--- reflect/reflect_test.go
+++ reflect/reflect_test.go
@@ -5,24 +5,151 @@ import (
 	"testing"
 )
 
+type Pessoa struct {
+	Nome   string
+	Perfil Perfil
+}
+
+type Perfil struct {
+	Idade  int
+	Cidade string
+}
+
 func TestPercorre(t *testing.T) {
-	esperado := "Chris"
-	var resultado []string
+	casos := []struct {
+		Nome              string
+		Entrada           interface{}
+		ChamadasEsperadas []string
+	}{
+		{
+			"Struct com um campo string",
+			struct {
+				Nome string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct com dois campos tipo string",
+			struct {
+				Nome   string
+				Cidade string
+			}{"Chris", "Londres"},
+			[]string{"Chris", "Londres"},
+		},
+		{
+			"Struct sem campo tipo string",
+			struct {
+				Nome  string
+				Idade int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Campos aninhados",
+			Pessoa{
+				"Chris",
+				Perfil{33, "Londres"},
+			},
+			[]string{"Chris", "Londres"},
+		},
+		{
+			"Ponteiros para coisas",
+			&Pessoa{
+				"Chris",
+				Perfil{33, "Londres"},
+			},
+			[]string{"Chris", "Londres"},
+		},
+		{
+			"Slices",
+			[]Perfil{
+				{33, "Londres"},
+				{34, "Reykjavik"},
+			},
+			[]string{"Londres", "Reykjavik"},
+		},
+		{
+			"Arrays",
+			[2]Perfil{
+				{33, "Londres"},
+				{34, "Reykjavík"},
+			},
+			[]string{"Londres", "Reykjavík"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
+	}
 
-	x := struct {
-		Nome string
-	}{esperado}
-
-	percorre(x, func(entrada string) {
-		resultado = append(resultado, entrada)
+	t.Run("com maps", func(t *testing.T) {
+		mapA := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+		var resultado []string
+		percorre(mapA, func(entrada string) {
+			resultado = append(resultado, entrada)
+		})
+		verificaSeContem(t, resultado, "Bar")
+		verificaSeContem(t, resultado, "Boz")
 	})
-	if resultado[0] != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado[0], esperado)
+
+	for _, teste := range casos {
+		t.Run(teste.Nome, func(t *testing.T) {
+			var resultado []string
+			percorre(teste.Entrada, func(entrada string) {
+				resultado = append(resultado, entrada)
+			})
+			if !reflect.DeepEqual(resultado, teste.ChamadasEsperadas) {
+				t.Errorf("resultado %v, esperado %v", resultado, teste.ChamadasEsperadas)
+			}
+		})
+	}
+}
+
+func verificaSeContem(t *testing.T, palheiro []string, agulha string) {
+	contem := false
+	for _, x := range palheiro {
+		if x == agulha {
+			contem = true
+		}
+	}
+	if !contem {
+		t.Errorf("esperava-se que %+v contivesse '%s', mas não continha", palheiro, agulha)
 	}
 }
 
 func percorre(x interface{}, fn func(entrada string)) {
+	valor := obtemValor(x)
+	percorreValor := func(valor reflect.Value) {
+		percorre(valor.Interface(), fn)
+	}
+	switch valor.Kind() {
+	case reflect.String:
+		fn(valor.String())
+	case reflect.Struct:
+		for i := 0; i < valor.NumField(); i++ {
+			percorreValor(valor.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < valor.Len(); i++ {
+			percorreValor(valor.Index(i))
+		}
+	case reflect.Map:
+		for _, chave := range valor.MapKeys() {
+			percorreValor(valor.MapIndex(chave))
+		}
+	}
+}
+func obtemValor(x interface{}) reflect.Value {
 	valor := reflect.ValueOf(x)
-	campo := valor.Field(0)
-	fn(campo.String())
+	if valor.Kind() == reflect.Ptr {
+		valor = valor.Elem()
+	}
+	return valor
 }
