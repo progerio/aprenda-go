f4fbb5d45a04c9fa8a98f981be4527bef431c1ce
Paulo Rogerio <paulo@intranet>
2025-10-06 08:52:46 -0300

    refactor: Improve Contador and test cases for concurrency safety

--- sync/contador.go
+++ sync/contador.go
@@ -1,13 +1,22 @@
 package sync
 
+import "sync"
+
 type Contador struct {
+	mu    sync.Mutex
 	valor int
 }
 
 func (c *Contador) Incrementar() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.valor++
 }
 
 func (c *Contador) Valor() int {
 	return c.valor
+}
+
+func NovoContador() *Contador {
+	return &Contador{}
 }
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestContador(t *testing.T) {
 	t.Run("incrementar o contador 3 vezes no valor 3", func(t *testing.T) {
@@ -9,11 +12,30 @@ func TestContador(t *testing.T) {
 		contador.Incrementar()
 		contador.Incrementar()
 
-		verificaContador(t, contador, 3)
+		verificaContador(t, &contador, 3)
 	})
+
+	t.Run("Roda concorrentemente em segurança", func(t *testing.T) {
+		contagemEsperada := 1000
+		contador := NovoContador()
+
+		var wg sync.WaitGroup
+		wg.Add(contagemEsperada)
+
+		for i := 0; i < contagemEsperada; i++ {
+			go func(w *sync.WaitGroup) {
+				contador.Incrementar()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		verificaContador(t, contador, contagemEsperada)
+	})
+
 }
 
-func verificaContador(t *testing.T, contador Contador, esperado int) {
+func verificaContador(t *testing.T, contador *Contador, esperado int) {
 	t.Helper()
 	if contador.Valor() != esperado {
 		t.Errorf("resultado %d, esperado %d", contador.Valor(), esperado)
