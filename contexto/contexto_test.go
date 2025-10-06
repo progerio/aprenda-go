24ba3caa1159dbec9e2b1a605562e4318342f4b0
Paulo Rogerio <paulo.rogeriobr>
2025-10-06 20:20:26 -0300

    feat: Learning context in golang

---
+++ contexto/contexto_test.go
@@ -0,0 +1,29 @@
+package contexto
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestHandler(t *testing.T) {
+	data := "olá, mundo"
+	svr := Server(&StubStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("resultado %s , esperado %s ", response.Body.String(), data)
+	}
+}
