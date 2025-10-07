4ee2c7f1b046ed360648e32a90872bfc011d2176
Paulo Rogerio <paulo@intranet>
2025-10-07 09:07:35 -0300

    refactor: Update Store interface and improve concurrency handling in tests

--- contexto/contexto.go
+++ contexto/contexto.go
@@ -1,31 +1,21 @@
 package contexto
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			return // todo: registre o erro como você quiser
 		}
-		fmt.Fprint(w, store.Fetch())
+		fmt.Fprint(w, data)
 	}
 }
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
--- contexto/contexto_test.go
+++ contexto/contexto_test.go
@@ -2,6 +2,7 @@ package contexto
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,57 +22,76 @@ func (s *StubStore) Cancel() {
 }
 
 type SpyStore struct {
-	response  string
-	cancelled bool
+	response string
+	t        *testing.T
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Microsecond)
-	return s.response
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store foi cancelado")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
+type SpyResponseWriter struct {
+	written bool
 }
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("não implementado")
+}
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestHandler(t *testing.T) {
 	data := "olá, mundo"
-	svr := Server(&StubStore{response: data})
-
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-
-	svr.ServeHTTP(response, request)
-
-	if response.Body.String() != data {
-		t.Errorf("resultado %s , esperado %s ", response.Body.String(), data)
-	}
-
-	t.Run("avisa a store para cancelar o trabalho se a requisição for cancelada  ", func(t *testing.T) {
-		store := &SpyStore{response: data}
-		svr := Server(store)
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		cancellingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(5*time.Millisecond, cancel)
-		request = request.WithContext(cancellingCtx)
-		response := httptest.NewRecorder()
-		svr.ServeHTTP(response, request)
-		if !store.cancelled {
-			t.Errorf("store não foi avisada para cancelar")
-		}
-	})
-
 	t.Run("retorna dados da store", func(t *testing.T) {
-		store := SpyStore{response: data}
-		svr := Server(&store)
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
 		svr.ServeHTTP(response, request)
 		if response.Body.String() != data {
 			t.Errorf(`resultado "%s", esperado "%s"`, response.Body.String(), data)
 		}
-		if store.cancelled {
-			t.Error("não deveria ter cancelado a store")
+	})
+
+	t.Run("avisa a store para cancelar o trabalho se a requisição for cancelada", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+
+		request = request.WithContext(cancellingCtx)
+		response := &SpyResponseWriter{}
+		svr.ServeHTTP(response, request)
+
+		if response.written {
+			t.Error("uma resposta não deveria ter sido escrita")
 		}
 	})
 }
